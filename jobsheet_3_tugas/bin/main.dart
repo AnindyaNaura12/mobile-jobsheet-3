3b54e120883927b4a68308fe7cc0bb7b99f191ea
07nawraa <anindyanauraanindya@gmail.com>
2026-03-10 14:43:31 +0700

    Commit 3: Menambahkan validasi input angka

--- jobsheet_3_tugas/bin/main.dart
+++ jobsheet_3_tugas/bin/main.dart
@@ -3,4 +3,9 @@ import 'dart:io';
 void main() {
   print("Masukkan angka:");
   int angka = int.parse(stdin.readLineSync()!);
+  if (angka <= 0) {
+    print("Masukkan angka lebih dari 0");
+  } else {
+    print ("Angka yang dimasukkan: $angka");
+  }
 }

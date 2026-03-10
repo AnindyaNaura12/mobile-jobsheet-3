071adf19cfcbe3f1cd7f1831fdf3f48bdea46e59
07nawraa <anindyanauraanindya@gmail.com>
2026-03-10 14:53:34 +0700

    Commit 5: Menambahkan fungsi cek bilangan prima

--- jobsheet_3_tugas/bin/main.dart
+++ jobsheet_3_tugas/bin/main.dart
@@ -26,3 +26,11 @@ void main() {
     print("Menu tidak tersedia");
   }
 }
+
+bool cekPrima(int angka) {
+  if (angka < 2) return false;
+  for (int i = 2; i <= angka ~/ 2; i++) {
+    if (angka % i == 0) return false;
+  }
+  return true;
+}
